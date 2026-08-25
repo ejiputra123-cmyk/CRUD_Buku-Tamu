ae4c265323d22b2db9edc484a62a8eb2b5289d5b
Eji Putra <ejiputra123@gmail.com>
2026-08-25 12:37:57 +0700

    menambahkan halaman hapus tamu dan fitur hapus data tamu

---
+++ hapus-tamu.php
@@ -0,0 +1,16 @@
+<?php
+// panggil file function.php
+require_once 'function.php';
+
+// jika ada id
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+    if (hapus_tamu($id) > 0) {
+        // jika data berhasil di hapus maka akan muncul alert
+        echo "<script>window.location.href='buku-tamu.php'</script>";
+    } else {
+        // jika gagal di hapus
+        echo "<script>alert('Data gagal di hapus!')</script>";
+    }
+}
+?>

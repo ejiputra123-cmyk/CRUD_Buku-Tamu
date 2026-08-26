74e358866a97a0c827c1e549d31b3717b173d932
Eji Putra <ejiputra123@gmail.com>
2026-08-26 17:22:23 +0700

    membuat dan menampilkan halaman user

--- hapus-tamu.php
+++ hapus-tamu.php
@@ -7,6 +7,8 @@ if (isset($_GET['id'])) {
     $id = $_GET['id'];
     if (hapus_tamu($id) > 0) {
         // jika data berhasil di hapus maka akan muncul alert
+        echo "<script>alert('Data berhasil di hapus!')</script>";
+        // redirect ke halaman buku-tamu.php
         echo "<script>window.location.href='buku-tamu.php'</script>";
     } else {
         // jika gagal di hapus

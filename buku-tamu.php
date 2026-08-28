b3ad23aa339f4162b48dc87c25dfda873bd8ae43
Eji Putra <ejiputra123@gmail.com>
2026-08-28 12:59:48 +0700

    menambahkan fitur upload gambar di buku_tamu

--- buku-tamu.php
+++ buku-tamu.php
@@ -126,7 +126,7 @@ $kodeTamu = $huruf . sprintf("%03s", $urutan);
         </button>
       </div>
         <div class="modal-body">
-            <form action="" method="post">
+            <form action="" method="post" enctype="multipart/form-data">
                 <input type="hidden" name="id_tamu" id="id_tamu" value="<?= $kodeTamu ?>">
                 <div class="form-group row">
                     <label for="nama_tamu" class="col-sm-3 col-form-label">Nama Tamu</label>
@@ -160,6 +160,13 @@ $kodeTamu = $huruf . sprintf("%03s", $urutan);
                     <label for="kepentingan" class="col-sm-3 col-form-label">Kepentingan</label>
                     <div class="col-sm-8">
                         <input type="text" class="form-control" id="kepentingan" name="kepentingan">
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="gambar" class="col-sm-3 col-form-label">Unggah Foto</label>
+                    <div class="custom-file col-sm-8">
+                        <input type="file" class="custom-file-input" id="gambar" name="gambar">
+                        <label class="custom-file-label" for="gambar">Choose file</label>
                     </div>
                 </div>
                     <div class="modal-footer">

3a0052f96a5be23328529405d28d31b4e6f1604f
Eji Putra <ejiputra123@gmail.com>
2026-08-28 11:34:07 +0700

    menambahkan fitur export laporan format excel

--- laporan.php
+++ laporan.php
@@ -1,6 +1,19 @@
 <?php
 include_once('templates/header.php');
-require_once('function.php')
+require_once('function.php');
+
+if (isset($_POST['tampilkan'])) {
+    $p_awal = $_POST['p_awal'];
+    $p_akhir = $_POST['p_akhir'];
+    
+    $link = "export-laporan.php?cari=true&p_awal=$p_awal&p_akhir=$p_akhir";
+    // query sesuai dengan keyword
+    $buku_tamu = query("SELECT * FROM buku_tamu WHERE tanggal BETWEEN '$p_awal' AND '$p_akhir' ");
+} else {
+    // query ambil semua data buku tamu
+    $buku_tamu = query("SELECT * FROM buku_tamu ORDER BY tanggal DESC");
+}
+
 ?>
 
 <!-- Begin Page Content -->
@@ -53,7 +66,12 @@ require_once('function.php')
 
     <div class="card shadow mb-4">
         <div class="card-header py-3">
-            <span class="text">Tabel Histori</span>
+            <a href="<?= isset($_POST['tampilkan']) ? $link : 'export-laporan.php'; ?>" target="_blank" class="btn btn-success btn-icon-split">
+                <span class="icon text-white-50">
+                    <i class="fas fa-file-excel"></i>
+                </span>
+                <span class="text">Export Laporan</span>
+            </a>
         </div>
         <div class="card-body">
             <div class="table-responsive">
@@ -72,13 +90,9 @@ require_once('function.php')
                     </thead>
                     <tbody>
                         <?php
-                        if (isset($_POST['tampilkan'])) {
-                            $p_awal = $_POST['p_awal'];
-                            $p_akhir = $_POST['p_akhir'];
                             // penomoran auto-increment
                             $no = 1;
-                            // Query untuk memanggil semua data dari tabel buku_tamu
-                            $buku_tamu = query("SELECT * FROM buku_tamu WHERE tanggal BETWEEN '$p_awal' AND '$p_akhir' ");
+
                             foreach ($buku_tamu as $tamu) : ?>
                                 <tr>
                                     <td><?= $no++; ?></td>
@@ -89,14 +103,12 @@ require_once('function.php')
                                     <td><?= $tamu['bertemu'] ?></td>
                                     <td><?= $tamu['kepentingan'] ?></td>
                                     <td>
-                                        <a class="btn btn-success" href="edit-tamu.php?id=<?= 
-                                        $tamu['id_tamu'] ?>">Ubah</a>
+                                        <a class="btn btn-success" href="edit-tamu.php?id=<?= $tamu['id_tamu'] ?>">Ubah</a>
                                         <a onclick="return confirm('Apakah anda yakin ingin menghapus data ini?')" class="btn btn-danger"
                                         href="hapus-tamu.php?id=<?= $tamu['id_tamu'] ?>">Hapus</a>
                                     </td>
                                 </tr>
                         <?php endforeach;
-                        }
                         ?>
                     </tbody>
                 </table>
